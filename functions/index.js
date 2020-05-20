a5df19a51b04a508413db90fc4b65e65475550c8
Nickolay Polyakov <nikolay.polyakov@ormco.com>
2020-05-20 11:42:53 +0300

    working pay function and logging of fail,check,recurring,pay

--- functions/index.js
+++ functions/index.js
@@ -1,78 +1,134 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
-
-const CryptoJS = require("crypto-js");
 const crypto = require("crypto")
-
-
 
 admin.initializeApp();
 const db = admin.firestore();
 
+const EventType = {
+    'Payment': 'Payment',
+    'Recurring': 'Recurring',
+    'Check': 'Check',
+    'Fail': 'Fail',
+}
+
+const SubscriptionStatus = {
+    'Active': 'Active',
+    'PastDue': 'PastDue',
+    'Cancelled': 'Cancelled',
+    'Rejected': 'Rejected',
+    'Expired': 'Expired',
+}
+
+const USER_COLLECTION = 'testUsers'
 
 
-// Cloud Payments Events 
-
+// security
 const hashInBase64 = (payload) => {
-    const secret = process.env.API_SECRET
-    // console.log("API - %s",secret);
-    // console.log("PAYLOAD - %s",payload);
-    // // TODO check IPs 
-    // const hash = CryptoJS.HmacSHA256(payload, secret);
-    // console.log("HASH - %s",hash);
-    // const res= CryptoJS.enc.Base64.stringify(hash);
-    // console.log("hashed result - %s",res)
+    const secret = functions.config().cloudpayments.api_secret;
 
     return crypto.createHmac("sha256", secret)
         .update(payload)
         .digest("base64");
+}
+
+const signedByCloudPayments = (request) => {
+    const HMAC = request.header('Content-HMAC');
+    const hashedBody = hashInBase64(Buffer.from(request.rawBody, 'utf-8'));
+    const res = hashedBody === HMAC;
+
+    if (!res) {
+        console.error("Checksum of POST body %s is not equal to Content-HMAC : %s", hashedBody, HMAC)
+    }
+    return res;
+}
+
+// logging to firebase
+
+const logUserEvent = async (event, eventType) => {
+    collection = USER_COLLECTION;
+    try {
+        const { AccountId } = event;
+        const userRef = db.collection(collection).doc(AccountId);
+
+        await userRef.update({
+            'subscription.events.cloudPayments': admin.firestore.FieldValue.arrayUnion(event)
+        })
+
+        console.log('Added document about: %s for user %s ', eventType, AccountId);
+    }
+    catch (e) {
+        console.error("Error happened", e.message);
+    }
 
 }
 
+// services
+const startSubscriptionForUser = async (AccountId) => {
+    console.log(USER_COLLECTION, `${AccountId}`, SubscriptionStatus.Active);
+    return db.collection(USER_COLLECTION).doc(`${AccountId}`).update({
+        'subscription.status': SubscriptionStatus.Active,
+    });
+}
 
-
-exports.pay = functions.https.onRequest(async (request, response) => {
+//  common middleware
+const executeEvent = async (request, response, execute) => {
     if (request.method !== 'POST') {
         response.send({ "code": 13 })
     }
     else {
         try {
-            const HMAC = request.header('Content-HMAC');
-            const hashedBody = hashInBase64(Buffer.from(request.rawBody, 'utf-8'));
-            if (hashedBody === HMAC) {
-                const { AccountId, Email, OperationType } = request.body;
-                const testSubPosting = db.collection('testSubPosting');
-                const documentReference = testSubPosting.doc(AccountId);
-                await documentReference.set({ AccountId, OperationType, Email, body: request.body })
+            if (signedByCloudPayments(request)) {
+                await execute();
                 response.send({ "code": 0 });
             }
             else {
-                console.error("Checksum of POST body %s is not equal to Content-HMAC : %s", hashedBody, HMAC)
                 response.status(401).send({ "code": 13 });
             }
         }
         catch (e) {
-            console.error("Error happened", e.message);
+            console.error("error happened", e.message);
             response.send({ "code": 13 })
         }
     }
+}
+
+//  API 
+exports.pay = functions.https.onRequest(async (request, response) => {
+    executeEvent(request, response, async () => {
+        await startSubscriptionForUser(request.body.AccountId);
+        await logUserEvent(request.body, EventType.Payment);
+    })
 });
 
 exports.recurrent = functions.https.onRequest(async (request, response) => {
-    if (request.method !== 'POST') {
-        response.send({ "code": 13 })
-    }
-    else {
-        try {
-            const { AccountId, Email, OperationType } = request.body;
-            const testSubPosting = db.collection('testSubPosting');
-            const documentReference = testSubPosting.doc(AccountId);
-            await documentReference.set({ subscription: { AccountId, OperationType, Email, body: request.body } })
-            response.send({ "code": 0 });
-        }
-        catch (e) {
-            response.send({ "code": 13 })
-        }
-    }
+    executeEvent(request, response, async () => {
+        await logUserEvent(request.body, EventType.Recurring);
+    })
 });
+
+exports.check = functions.https.onRequest(async (request, response) => {
+    executeEvent(request, response, async () => {
+        await logUserEvent(request.body, EventType.Check);
+    })
+});
+
+exports.fail = functions.https.onRequest(async (request, response) => {
+    executeEvent(request, response, async () => {
+        await logUserEvent(request.body, EventType.Fail);
+    })
+});
+
+
+// tests
+const testActivateSubscription = async () => {
+    try {
+        await startSubscriptionForUser("1407529623");
+    }
+    catch (e) {
+        console.error("error", e.message);
+    }
+
+}
+
+testActivateSubscription();
